dbe65b5d178d952088ee867f6e012e63310151ce
protolambda <proto@protolambda.com>
2024-01-21 12:11:38 -0500

    lighthouse CLI tweaks

--- ops-bedrock/l1-lighthouse-bn-entrypoint.sh
+++ ops-bedrock/l1-lighthouse-bn-entrypoint.sh
@@ -4,9 +4,19 @@ set -exu
 exec /usr/local/bin/lighthouse \
   bn \
 	--datadir="/db" \
-	--http \
 	--disable-peer-scoring \
 	--disable-packet-filter \
+	--enable-private-discovery \
+	--staking \
+	--http \
+	--http-address=0.0.0.0 \
+	--http-port=5052 \
+	--validator-monitor-auto \
+	--http-allow-origin \
+	--listen-address=0.0.0.0 \
+	--port=9000 \
+	--target-peers=0 \
+	--testnet-dir=/genesis \
   --execution-endpoint="${LH_EXECUTION_ENDPOINT}" \
 	--execution-jwt=/config/jwt-secret.txt \
 	"$@"
--- ops-bedrock/l1-lighthouse-vc-entrypoint.sh
+++ ops-bedrock/l1-lighthouse-vc-entrypoint.sh
@@ -5,4 +5,7 @@ exec /usr/local/bin/lighthouse \
   vc \
 	--datadir="/db" \
   --beacon-nodes="${LH_BEACON_NODES}" \
+  --testnet-dir=/genesis \
+  --secrets-dir=/validators/secrets \
+  --validators-dir=/validators/keystores \
 	"$@"
