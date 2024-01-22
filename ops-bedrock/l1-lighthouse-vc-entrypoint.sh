0b763dfa50f626376c79caf70acb0bd6c56e7a73
protolambda <proto@protolambda.com>
2024-01-22 11:10:56 -0600

    devnet: fix validator keys loading

--- ops-bedrock/l1-lighthouse-vc-entrypoint.sh
+++ ops-bedrock/l1-lighthouse-vc-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -exu
 
+# Copy the keys and secrets into place:
+# We don't mount directly into place, to prevent the container from writing lock-files / slashing-db / etc.
+# back into the host, which could affect future fresh devnet runs if not cleaned up.
+cp -r /validator_setup/validators /db/validators
+cp -r /validator_setup/secrets /db/secrets
+
 exec /usr/local/bin/lighthouse \
   vc \
 	--datadir="/db" \
   --beacon-nodes="${LH_BEACON_NODES}" \
   --testnet-dir=/genesis \
-  --secrets-dir=/validators/secrets \
-  --validators-dir=/validators/keystores \
+  --init-slashing-protection \
 	"$@"
